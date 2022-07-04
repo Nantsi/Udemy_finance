f01dd220172ec371abfef6037cae67d7f8f68095
Ubuntu <ubuntu@ip-172-31-14-144.ec2.internal>
2022-07-04 06:35:41 +0000

    Course 9 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+ 
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
+  
+  def friends
+    @cfriends = current_user.friends
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+  
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friend = params[:friend]
+      @cfriends = current_user.friends
+      if @friends
+          render 'users/friends'
+      else
+          flash[:alert] = "Couldn't find user"
+          redirect_to friends_path
+      end
+    else
+        flash[:alert] = "Please enter a search"
+        redirect_to friends_path
+    end
+  end
+    
 end
